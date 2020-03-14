5dcafbebbb06ff3fc73f54f50926de592105c2fb
rbargues <rbargues@gmail.com>
2020-03-14 19:59:42 -0400

    initial setup w/ webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = env => {
+console.log(env)
+return {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    mode: env.NODE_ENV,
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            } 
+          }, 
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+            ],
+              }
+        ]
+      },
+      devServer: {
+          publicPath:'/build/', 
+          proxy: {
+              '/': {
+                target: 'http://localhost:3000', 
+            }
+        }
+      }
+    }
+}
