6b1a95e57131cb1719554ed386901d1e280c8adb
rbargues <rbargues@gmail.com>
2020-03-14 21:23:26 -0400

    added collision logic for game over

--- client/reducers/mapReducer.js
+++ client/reducers/mapReducer.js
@@ -5,15 +5,42 @@ const initialState = {
     x2: 0,
     y1: 0,
     y2: 0
-  }]
+  }],
+  distanceBetweenPipes: 50,
+  gameOver: false
 };
 
 function mapReducer(state = initialState, action) {
   switch(action.type) {
-    case "COLLISION": {
-      return state = {...state, ...action.payload};
+    case "CHECKCOLLISION": {
+      const { birdY, birdHeight, pipes } = action.payload;
+      const birdTopY = birdY + birdHeight;
+      let gameOver = false;
+      const pipe = pipes[0]; //{x1,x2,y1,y2} left right bottom top
+      //only the first pipe can hurt the bird
+      if (birdTopY >= pipe.y2) {
+        gameOver = true;
+      }
+      else if (birdY >= pipe.y2) {
+        gameOver = true;
+      }
+      else if (birdTopY <= pipe.y1) {
+        gameOver = true;
+      }
+      else if (birdHeight <= pipe.y1) {
+        gameOver = true;
+      }
+
+      const newState = {
+        gameOver
+      };
+      
+      return state = {...state, ...newState};
     }
     case "NEWPIPE": {
+
+
+
       return state = {...state, ...action.payload};
     }
     default:
