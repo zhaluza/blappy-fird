c78aa8cb89f31c5cdeedc3d761e37e6e220a2e01
rbargues <rbargues@gmail.com>
2020-03-14 21:03:13 -0400

    moved logic to reducers to allow for modularization

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,36 +1,39 @@
 // we will call this action 
 export const moving = (yPosition, gravity, flapping, maxPosition) => {
-  yPosition += gravity;
-  if (maxPosition) {
-    if (yPosition >= maxPosition) {
-      flapping = false;
-      maxPosition = undefined;
-    }
-  }
-  if (flapping) {
-    gravity += 10; //will need adjust for gampelay
-  } 
-  else {
-    gravity -= 5;
-  }
+  // yPosition += gravity;
+  // if (maxPosition) {
+  //   if (yPosition >= maxPosition) {
+  //     flapping = false;
+  //     maxPosition = undefined;
+  //   }
+  // }
+  // if (flapping) {
+  //   gravity += 10; //will need adjust for gampelay
+  // } 
+  // else {
+  //   gravity -= 5;
+  // }
   return {
     type: "FLAPPING",
     payload: {
       yPosition: yPosition,
       gravity: gravity,
-      flapping: flapping
+      flapping: flapping,
+      maxPosition: maxPosition
     }
   };
 };
 
 export const jump = (yPosition, maxPosition, flapping, jumpHeight) => {
-  flapping = true;
-  maxPosition = yPosition + jumpHeight;
+  // flapping = true;
+  // maxPosition = yPosition + jumpHeight;
   return {
     type: "FLAP",
     payload: {
       maxPosition: maxPosition,
-      flapping: flapping
+      flapping: flapping,
+      yPosition: yPosition,
+      jumpHeight: jumpHeight
     }
   };
 };
--- client/reducers/birdReducer.js
+++ client/reducers/birdReducer.js
@@ -8,10 +8,30 @@ const initialState = {
 function birdReducer (state = initialState, action) {
   switch(action.type) {
     case "FLAPPING": {
-      return {...state, ...action.payload};
+      //do stuff based on action.payload
+      const {yPosition, gravity, flapping, maxPosition} = action.payload;
+      yPosition += gravity;
+      if (maxPosition) {
+        if (yPosition >= maxPosition) {
+          flapping = false;
+          maxPosition = undefined;
+        }
+      }
+      if (flapping) {
+        gravity += 10; //will need adjust for gampelay
+      } 
+      else {
+        gravity -= 5;
+      }
+      const newState = {yPosition, gravity, flapping};
+      return {...state, ...newState};
     }
     case "FLAP": {
-      return {...state, ...action.payload};
+      const {yPosition, maxPosition, flapping, jumpHeight} = action.payload;
+      flapping = true;
+      maxPosition = yPosition + jumpHeight;
+      const newState = {flapping, maxPosition};
+      return {...state, ...newState};
     }
     default:
       return state;
